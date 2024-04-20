58c7c4720e1290d1c68031301642df8a0eab49b5
Jerome Pourra <j.pourra.perso@gmail.com>
2024-04-20 15:26:43 +0200

    css base & skin load dynamically

--- widget.js
+++ widget.js
@@ -1,16 +1,57 @@
 export class Widget {
 
-	/**
-	 * @type {JQuery<HTMLElement>}
-	 */
+	/** @type {JQuery<HTMLElement>} */
 	$m_root = $(); // root widget container
 
-	/**
-	 * @type {string}
-	 */
+	/** @type {string} */
 	m_skin = "dfo"; // default skin
 
-	constructor() {}
+	/** @type {string} */
+	m_widgetName;
+
+	constructor() {
+		this.m_widgetName = this.constructor.name;
+	}
+
+	/**
+	 * @param {string} v
+	 */
+	skin(v) {
+		this.m_skin = v;
+		return this;
+	}
+
+	/**
+	 * @param {string} basepath 
+	 * @returns {void}
+	 */
+	loadCss(basepath = ".") {
+		let widgetPath = this.m_widgetName.toLowerCase();
+		let cssPath = `${basepath}/${widgetPath}/${widgetPath}.css`
+		this.appendCss(cssPath);
+	}
+
+	/**
+	 * @param {string} basepath 
+	 * @returns {void}
+	 */
+	loadSkin(basepath = ".") {
+		let widgetPath = this.m_widgetName.toLowerCase();
+		let skinPath = `${basepath}/${widgetPath}/skins/${this.m_skin}.css`
+		this.appendCss(skinPath);
+	}
+
+	/**
+	 * @param {string} path 
+	 */
+	appendCss(path) {
+		setTimeout(() => {
+			if ($(`link[href="${path}"]`).length === 0) {
+				let $link = $(`<link rel="stylesheet" href="${path}"></link>`);
+				$(document.head).append($link);
+			}
+		}, 500);
+	}
 
 	/**
 	 * This method is just a way to force child class overring it
